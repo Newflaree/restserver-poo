73430e9fbbd791dc6562eb294ab8e2394b236c7f
Newflare <camiloj.lopez79@gmail.com>
2021-02-10 05:25:17 -0300

    Primer commit

---
+++ routes/user.routes.js
@@ -0,0 +1,20 @@
+const { Router } = require( 'express' );
+
+const { 
+  getUsers, 
+  postUser, 
+  putUser, 
+  patchUser, 
+  deleteUser
+} = require('../controllers/user.controller');
+
+const router = Router();
+
+router.get( '/', getUsers );
+router.post( '/', postUser );
+router.put( '/:id', putUser );
+router.patch( '/', patchUser );
+router.delete( '/', deleteUser);
+
+
+module.exports = router;
